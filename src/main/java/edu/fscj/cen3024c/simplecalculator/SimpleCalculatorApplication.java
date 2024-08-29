28d55e51eaaf85c3cfda41a7bf5b0760f6334cc0
Smith <S3731317@students.fscj.edu>
2024-08-29 13:00:21 -0400

    added Simple math Class to Cole repoistory

--- src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculatorApplication.java
+++ src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculatorApplication.java
@@ -2,6 +2,7 @@
 // D. Singletary
 // 1/9/2024
 // Template for calculator application
+// David Smith
 
 package edu.fscj.cen3024c.simplecalculator;
 
@@ -16,19 +17,19 @@ public class SimpleCalculatorApplication {
         // Test the multiply function
         int number1 = 10; // This can be input from the user or arguments
         int number2 = 5; // This can be input from the user or arguments
-        int result = multiply(number1, number2);
+        int result = SimpleMath.multiply(number1, number2);
         System.out.println("Multiply result is: " + result);
 
         // Test the divide function
-        result = divide(number1, number2);
+        result = SimpleMath.divide(number1, number2);
         System.out.println("Divide result is: " + result);
 
         // Test the add function
-        result = add(number1, number2);
+        result = SimpleMath.add(number1, number2);
         System.out.println("Add result is: " + result);
 
         // Test the subtract function
-        result = subtract(number1, number2);
+        result = SimpleMath.subtract(number1, number2);
         System.out.println("Add result is: " + result);
     }
 }
---
+++ src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleMath.java
@@ -0,0 +1,24 @@
+// SimpleMath.java
+// David Smith
+// 8/29/2024
+// Does the math for simpleCalculatorApplication
+
+package edu.fscj.cen3024c.simplecalculator;
+
+public class SimpleMath {
+    public static int multiply (int a, int b)
+    {return a * b;
+    };
+
+    public static int divide (int a, int b)
+    {return a / b;
+    }
+
+    public static int add (int a, int b)
+    {return a + b;
+    }
+
+    public static int subtract (int a, int b)
+    {return a - b;
+    }
+}
